6de82eca01e41d1742724ef35da3f4ef839de9d9
Shiraj Shrestha <shresthashiraj333@gmail.com>
2024-09-09 21:39:38 +0545

    Add Quote array

---
+++ script.js
@@ -0,0 +1,46 @@
+const quotes = {
+  science: [
+    {
+      quote: ` "The important thing is to never stop questioning."`,
+      author: "- Albert Einstein",
+    },
+    {
+      quote: `"Science is a way of thinking much more than it is a body of knowledge." `,
+      author: "- Carl Sagan",
+    },
+    {
+      quote: `"Science and everyday life cannot and should not be separated." `,
+      author: "Rosalind Franklin",
+    },
+    {
+      quote: `"Science is not only a disciple of reason but also one of romance and passion." `,
+      author: "- Stephen Hawking",
+    },
+    {
+      quote: `"The good thing about science is that it's true whether or not you believe in it." `,
+      author: "- Neil deGrasse Tyson",
+    },
+  ],
+  motivation: [
+    {
+      quote: `"The only way to do great work is to love what you do."`,
+      author: "- Steve Jobs",
+    },
+    {
+      quote: `"Success is not the key to happiness. Happiness is the key to success."`,
+      author: " - Albert Schweitzer",
+    },
+    {
+      quote: `"Don't watch the clock; do what it does. Keep going."`,
+      author: "- Sam Levenson",
+    },
+    {
+      quote: `"The future depends on what you do today."`,
+      author: "- Mahatma Gandhi",
+    },
+    {
+      quote: `"Your limitation—it's only your imagination." `,
+      author: "- Moshin Khan",
+    },
+  ],
+};
